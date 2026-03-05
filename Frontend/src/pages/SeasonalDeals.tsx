ac3b2bfaadf96d035c1cf5dd5f5e4b33f5486eb3
Suman Kunwar <sumankunwar755@gmail.com>
2026-03-05 15:36:46 +0545

    updated

--- Frontend/src/pages/SeasonalDeals.tsx
+++ Frontend/src/pages/SeasonalDeals.tsx
@@ -7,7 +7,7 @@ const SeasonalDeals = () => {
     <TripListingPage
       title="Seasonal Deals & Offers"
       tagline="Grab the Best Deals"
-      subtitle="Limited time offers on the most amazing destinations."
+      subtitle="Limited time offers on the most amazing destinations across the world."
       description="Don't miss out on our exclusive seasonal deals! We've handpicked the best destinations with incredible discounts just for you. Book now and save big on your next adventure. These deals won't last forever..."
       heroImage="https://res.cloudinary.com/dihev9qxc/image/upload/v1772357854/f9b7a60c-3000-4447-ade9-12a71b5c9cee-Photoroom_z6b6zf.png"
       tripCategory="deals"
